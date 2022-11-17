67c3f1c42b04729d50b7d534676108dcf92e8f32
ly3012 <71602612+ly3012@users.noreply.github.com>
2022-11-17 23:48:37 +0700

    #10 create nav, table data, loop ejs

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -3,6 +3,7 @@ import mysql from 'mysql2';
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
+    password: '301201v++',
     database: 'nodejsbasic'
 });
 
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -7,7 +7,7 @@ let getHomePage = (req, res) => {
         'SELECT * FROM `users`',
         function (err, results, fields) {
             // Truyền dữ liệu ra index.ejs(dữ liệu dạng json tham số thứ 2 của res.render)
-            return res.render('index.ejs', ({ dataUser: JSON.stringify(results) }));
+            return res.render('index.ejs', ({ dataUser: results }));
         }
     );
 }
