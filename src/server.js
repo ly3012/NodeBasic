de3bf5f4d36d867d58db2b57dc68dcee37f0418b
ly3012 <71602612+ly3012@users.noreply.github.com>
2022-11-17 12:07:26 +0700

    Router & MVC

--- src/server.js
+++ src/server.js
@@ -1,28 +1,14 @@
 import { render } from 'ejs';
 import express from 'express'
 import configViewEngine from './configs/viewEngine'
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv'
+import initWebRoute from './route/web'
 dotenv.config();
 const app = express()
 const port = process.env.PORT || 8080;
 
 configViewEngine(app);
-
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-    // res.send('Hello World voi hoi dan IT va Eric!')
-    // res.sendFile(path.join(__dirname, './index.html'))
-})
-
-app.get('/about', (req, res) => {
-    res.send(`I'm Ly`)
-})
-
-app.get('/a', (req, res) => {
-    res.sendFile(path.join(__dirname, '../src', 'index.html'));
-    // res.sendFile(path.join(__dirname, '../public', 'index1.html'));
-})
-
+initWebRoute(app);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
